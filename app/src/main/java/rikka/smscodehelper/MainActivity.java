ca1df651b3e1ee0ac536d804cbe62263955d4aa2
RikkaW <s574204775@gmail.com>
2015-12-08 19:59:42 +0800

    first commit

--- app/src/main/java/rikka/smscodehelper/MainActivity.java
+++ app/src/main/java/rikka/smscodehelper/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
