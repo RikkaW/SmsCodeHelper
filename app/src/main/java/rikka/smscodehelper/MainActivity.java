d8eb66153074df0e2a62938c1ce268a7cb0a9899
RikkaW <rikka@shizuku.moe>
2018-08-18 23:03:34 +0800

    target 28

--- app/src/main/java/rikka/smscodehelper/MainActivity.java
+++ app/src/main/java/rikka/smscodehelper/MainActivity.java
@@ -1,82 +1,61 @@
 package rikka.smscodehelper;
 
 import android.Manifest;
-import android.content.DialogInterface;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.DialogStyle);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                .setOnCancelListener(dialog -> finish())
+                .setCancelable(false);
 
-        builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
-            @Override
-            public void onCancel(DialogInterface dialog) {
-                finish();
-            }
-        });
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
-                ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
             builder.setMessage(R.string.tip_m);
-            builder.setPositiveButton(R.string.get_permission, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    getPermission(Manifest.permission.RECEIVE_SMS);
-                }
-            });
+            builder.setPositiveButton(R.string.get_permission, (dialog, which) -> requestPermission());
+            builder.setNegativeButton(R.string.exit, (dialog, which) -> finish());
         } else {
             builder.setMessage(getString(R.string.tip));
-            builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    hideLauncher();
-                    finish();
-                }
+            builder.setPositiveButton(android.R.string.ok, (dialog, which) -> {
+                hideFromLauncher();
+                finish();
             });
         }
 
-        AlertDialog alertDialog = builder.create();
-        alertDialog.setCanceledOnTouchOutside(false);
-        alertDialog.show();
+        builder.show();
     }
 
-    private void getPermission(String permission) {
-        if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{permission}, 0);
+    private void requestPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECEIVE_SMS}, 0);
         }
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
             case 0:
                 if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.DialogStyle);
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
-                    builder.setTitle("OAQ");
                     builder.setMessage(R.string.permission_denied);
-                    builder.setPositiveButton(R.string.exit, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            finish();
-                        }
-                    });
-                    builder.create().show();
+                    builder.setPositiveButton(R.string.exit, (dialog, which) -> finish());
+                    builder.show();
                 } else {
+                    hideFromLauncher();
                     finish();
-                    hideLauncher();
                 }
                 break;
             default:
@@ -84,8 +63,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void hideLauncher() {
-        PackageManager p = getApplicationContext().getPackageManager();
-        p.setComponentEnabledSetting(getComponentName(), PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+    private void hideFromLauncher() {
+        PackageManager pm = getApplicationContext().getPackageManager();
+        pm.setComponentEnabledSetting(getComponentName(), PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
     }
 }
